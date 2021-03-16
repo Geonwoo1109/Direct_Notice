51e3f766e750c257406c43e76de947eeb89e5b8d
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-03-16 23:17:19 +0900

    Create Where Is My Notice Link?.js

---
+++ Where Is My Notice Link?.js
@@ -0,0 +1,51 @@
+var json = {
+    "_id": "20487",
+    "id": "2474360030928447488",
+    "type": "98",
+    "chat_id": "18303788426883321",
+    "user_id": "8122404112528045171",
+    "message": "[글 공유] .eval \na = \"a1b2c3d4\";\na.replace(/[0-9]/g,\"\");\n\n//abcd\n\n//https://developer.mozilla.org/ko/docs/W...",
+    "attachment": {
+        "os": [
+            {
+                "st": 2,
+                "t": 3,
+                "u": {
+                    "id": 8122404112528045000
+                }
+            },
+            {
+                "ct": ".eval \na = \"a1b2c3d4\";\na.replace(/[0-9]/g,\"\");\n\n//abcd\n\n//https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide/Regular_Expressions",
+                "jct": "[{\"text\":\".eval \\na = \\\"a1b2c3d4\\\";\\na.replace(/[0-9]/g,\\\"\\\");\\n\\n//abcd\\n\\n//https://developer.mozilla.org/ko/docs/Web/JavaScript/Guide/Regular_Expressions\",\"type\":\"text\"}]",
+                "t": 1
+            },
+            {
+                "ct": "{\"url\":\"https:\\/\\/developer.mozilla.org\\/ko\\/docs\\/Web\\/JavaScript\\/Guide\\/Regular_Expressions\",\"canonicalUrl\":\"https:\\/\\/developer.mozilla.org\\/ko\\/docs\\/Web\\/JavaScript\\/Guide\\/Regular_Expressions\",\"contentType\":\"text\\/html; charset=utf-8\",\"title\":\"정규 표현식 - JavaScript | MDN\",\"description\":\"\",\"suspected\":\"0\"}",
+                "t": 10
+            },
+            {
+                "st": 1,
+                "t": 2,
+                "url": "kakaomoim://post?referer=b&chat_id=18303788426883321&post_id=tIvJHh6BLGMhQ6zJ6riq"
+            }
+        ]
+    },
+    "created_at": "1615903901",
+    "deleted_at": "0",
+    "client_message_id": "105899878080752306",
+    "prev_id": "2474298611422148608",
+    "referer": "0",
+    "supplement": null,
+    "v": {
+        "notDecoded": false,
+        "origin": "MSG",
+        "c": "03-16 23:11:40",
+        "isSingleDefaultEmoticon": false,
+        "defaultEmoticonsCount": 0,
+        "isMine": false,
+        "enc": 29
+    }
+};
+
+var Url_Link = "g-o.kro.kr/?" + json.attachment.os[3].url;
+
